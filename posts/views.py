43487994ca3f6d9db474cca829a6db996f39ed38
Eden Zhang <53964412+Velocity9013@users.noreply.github.com>
2020-07-24 21:26:29 +1200

    Add Login Restricted to Some Views

--- posts/views.py
+++ posts/views.py
@@ -8,14 +8,22 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
 
 from .forms import SignUpForm, PostCreateForm
 
 # Create your views here.
+class UploaderRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().uploader != self.request.user:
+            return HttpResponseForbidden()
+        return super(UploaderRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
 def sign_up(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -69,29 +77,33 @@ def post_create(request):
 
 class PostView(DetailView):
     model = Post
+    context_object_name = "post"
     template_name = 'posts/post.html'
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UploaderRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'posts/post_update_form.html'
+    login_url = reverse_lazy('login')
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, UploaderRequiredMixin, DeleteView):
     model = Post
     success_url = '/'
+    login_url = reverse_lazy('login')
 
 
 def profile(request, username):
     user = get_object_or_404(get_user_model(), username=username)
     return render(request, 'posts/profile.html', {'user': user})
 
-class ProfileUpdate(UpdateView):
+class ProfileUpdate(LoginRequiredMixin, UpdateView):
     model = Profile
     fields = ['image', 'bio', 'location']
     template_name = 'posts/profile_update_form.html'
+    login_url = reverse_lazy('login')
 
     def get_object(self, queryset=None):
-        return Profile  .objects.get(user=self.request.user)
+        return Profile.objects.get(user=self.request.user)
 
 
 class SearchView(ListView):
