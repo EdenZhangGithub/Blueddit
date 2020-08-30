ff5ac6bcec91595ca60b7254a713e2a5483ab064
Eden Zhang <53964412+Velocity9013@users.noreply.github.com>
2020-08-30 14:52:53 +1200

    Change All URL with accounts/ to u/

--- posts/urls.py
+++ posts/urls.py
@@ -6,13 +6,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('accounts/signup/', views.sign_up, name='signup'),
-    path('accounts/login/', views.Login.as_view(), name='login'),
-    path('accounts/profile/<username>/', views.profile, name='profile'),
-    path('accounts/update-profile/', views.ProfileUpdate.as_view(), name='profile_update'),
+    path('u/signup/', views.sign_up, name='signup'),
+    path('u/login/', views.Login.as_view(), name='login'),
+    path('u/profile/<username>/', views.profile, name='profile'),
+    path('u/update-profile/', views.ProfileUpdate.as_view(), name='profile_update'),
     path('b/create/', views.CommunityCreate.as_view(), name='community_create'),
     path('b/<slug:slug>/', views.CommunityView.as_view(), name='community'),
-    path('posts/<int:pk>/', views.PostView.as_view(), name='post'),
+    path('b/posts/<int:pk>/', views.PostView.as_view(), name='post'),
     path('posts/create/', views.post_create, name='post_create'),
     path('posts/update-post/<int:pk>/', views.PostUpdate.as_view(), name='post_update'),
     path('posts/<int:pk>/delete-post/', views.PostDelete.as_view(), name='post_delete'),
